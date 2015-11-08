406f57488c5a01a7084cf42afeeea9a0eea68237
lq150415 <lq150415@gmail.com>
2015-11-08 09:35:14 -0400

    Mas cambios

---
+++ routes/ejemplo14.js
@@ -0,0 +1,21 @@
+var x=0;
+if(x){
+	console.log("true");
+
+}
+else {
+	console.log("false");
+
+}
+console.log("ejemplo b");
+
+console.log(x?"true":"false");
+
+
+
+
+
+
+
+console.log(x!==1?"true":"false");
+
